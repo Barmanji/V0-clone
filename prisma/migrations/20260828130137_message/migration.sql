dcfc3da7242740859122371071943228c2c6ab34
Ajay Barman <barmanjiaj@gmail.com>
2026-08-28 18:43:35 +0530

    feat(Migration): Add prisma message schema

---
+++ prisma/migrations/20260828130137_message/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "MessageRole" AS ENUM ('USER', 'ASSISTANT');
+
+-- CreateEnum
+CREATE TYPE "MessageType" AS ENUM ('RESULT', 'ERROR');
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "role" "MessageRole" NOT NULL,
+    "type" "MessageType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "projectId" TEXT NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
