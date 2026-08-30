798a2098b0df5fb02805d47c09912844843e0d0d
Ajay Barman <barmanjiaj@gmail.com>
2026-08-30 22:33:51 +0530

    feat(DB): Add prisma usage and fragment schema

---
+++ prisma/migrations/20260828131408_fragment_schema/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Fragment" (
+    "id" TEXT NOT NULL,
+    "messageId" TEXT NOT NULL,
+    "sandboxUrl" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "files" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Fragment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Fragment_messageId_key" ON "Fragment"("messageId");
+
+-- AddForeignKey
+ALTER TABLE "Fragment" ADD CONSTRAINT "Fragment_messageId_fkey" FOREIGN KEY ("messageId") REFERENCES "Message"("id") ON DELETE CASCADE ON UPDATE CASCADE;
