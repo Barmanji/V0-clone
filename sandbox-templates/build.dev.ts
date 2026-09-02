b27a8d95f67fd0f0e3d81bba9bdff4dc4fc7e3ce
Ajay Barman <barmanjiaj@gmail.com>
2026-09-02 20:28:27 +0530

    fix(e2b): add latest shadcn in tempelate

--- sandbox-templates/build.dev.ts
+++ sandbox-templates/build.dev.ts
@@ -1,9 +1,25 @@
-import { Template, defaultBuildLogger } from 'e2b'
-import { template } from './template'
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+import { Template, defaultBuildLogger } from "e2b";
+import { template } from "./template";
 
 async function main() {
-  await Template.build(template, 'v0-clone-build-dev', {
+  const apiKey = process.env.E2B_API_KEY;
+
+  if (!apiKey) {
+    throw new Error("E2B_API_KEY is not defined in your .env file.");
+  }
+
+  await Template.build(template, "v0-clone-build-dev", {
     onBuildLogs: defaultBuildLogger(),
+    apiKey: apiKey,
   });
 }
 
